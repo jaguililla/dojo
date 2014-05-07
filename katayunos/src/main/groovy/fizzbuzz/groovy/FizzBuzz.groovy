c5cacba9d49d932e2d325e7a0b6a04ca7cb7c9df
jamming <juanjoaguililla@gmail.com>
2014-05-07 09:05:24 +0200

    Changes to the FizzBuzz kata (Groovy version).

--- katayunos/src/main/groovy/fizzbuzz/groovy/FizzBuzz.groovy
+++ katayunos/src/main/groovy/fizzbuzz/groovy/FizzBuzz.groovy
@@ -1,21 +1,25 @@
 package fizzbuzz.groovy
 
+import groovy.transform.CompileStatic
+
 /**
  * http://www.solveet.com/exercises/Kata-FizzBuzz/11
  */
 class FizzBuzz {
-    def static List<String> fizzBuzz = (1..100).collect {
-        String strNumber = it.toString()
-        boolean mod3 = it % 3 == 0, mod5 = it % 5 == 0
-        boolean has3 = strNumber.contains ('3'), has5 = strNumber.contains ('5')
+    @CompileStatic private static List<String> fizzBuzz () {
+        (1..100).collect {
+            int number = (int)it // Required to compile statically
+            String strNumber = it.toString()
+            boolean mod3 = number % 3 == 0, mod5 = number % 5 == 0
+            boolean has3 = strNumber.contains ('3'), has5 = strNumber.contains ('5')
 
-        if ((mod3 && mod5) || (has3 && has5))
-            'FizzBuzz'
-        else if ((mod3) || (has3))
-            'Fizz'
-        else if ((mod5) || (has5))
-            'Buzz'
-        else
-            strNumber
+            if ((mod3 && mod5) || (has3 && has5)) 'FizzBuzz'
+            else if ((mod3) || (has3)) 'Fizz'
+            else if ((mod5) || (has5)) 'Buzz'
+            else strNumber
+        }
     }
+
+    // Syntactic sugar for the tests
+    def static List<String> fizzBuzz = fizzBuzz ()
 }
