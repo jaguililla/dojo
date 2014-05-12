5f4188d5ec0045700bec067abc613c96263e69ab
jamming <juanjoaguililla@gmail.com>
2014-05-13 00:09:18 +0200

    Remove SBT files (now using Gradle).

--- katayunos/build.sbt
+++
@@ -1,6 +0,0 @@
-name := "katayunos"
-
-version := "0.0-SNAPSHOT"
-
-libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
-
--- katayunos/project/build.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-import Keys._
-
-object ApplicationBuild extends Build {
-  name := "katayunos"
-  version := "0.0-SNAPSHOT"
-  libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
-}
