bf52fa6b7c072cc24989db20acb6a9177021c6e3
jamming <juanjoaguililla@gmail.com>
2015-11-23 19:59:51 +0100

    Add example of immutable beans in Java

--- demos/src/main/java/demos/InmutableExample.java
+++ demos/src/main/java/demos/InmutableExample.java
@@ -15,20 +15,24 @@
 package demos;
 
 import static demos.Address.address;
+import static demos.Car.car;
 import static demos.ObjectUtils.equal;
 import static demos.ObjectUtils.stringOf;
 import static demos.ObjectUtils.print;
 import static demos.Person.namedPerson;
 import static demos.Person.person;
 import static java.lang.String.format;
+import static java.util.Arrays.asList;
 import static java.util.Objects.hash;
 import static java.lang.System.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
 
 class ObjectUtils {
-    public static boolean equal (Object a, Object b) { return Objects.equals (a, b); }
+    public static <T> boolean equal (T a, T b) { return Objects.equals (a, b); }
 
     public static String stringOf (Object a) { return Objects.toString (a); }
 
@@ -50,7 +54,7 @@ class Address {
         return new Address (null, null, null);
     }
 
-    private final String streetAddress, city, state;
+    public final String streetAddress, city, state;
 
     private Address (final String streetAddress, final String city, final String state) {
         this.streetAddress = streetAddress;
@@ -120,9 +124,25 @@ class Address {
     }
 }
 
+class Car {
+    public static Car car (String brand, String model) {
+        return new Car (brand, model);
+    }
+
+    public final String brand, model;
+
+    private Car (String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public Car brand (String brand) { return new Car (brand, model); }
+    public Car model (String model) { return new Car (brand, model); }
+}
 class Person {
     public static Person person () {
-        return new Person (null, null, null, null, null, false, false, false, null);
+        return new Person (
+            null, null, null, null, null, false, false, false, null, new ArrayList<>());
     }
 
     public static Person person (Function<Person, Person> builder) {
@@ -131,17 +151,21 @@ class Person {
     }
 
     public static Person namedPerson (String firstName, String middleName, String lastName) {
-        return new Person (lastName, firstName, middleName, null, null, false, false, false, null);
+        return new Person (
+            lastName, firstName, middleName,
+            null, null, false, false, false, null, new ArrayList<>());
     }
 
-    private final String lastName, firstName, middleName;
-    private final String salutation;
-    private final String suffix;
-    private final boolean isFemale;
-    private final boolean isEmployed;
-    private final boolean isHomeOwner;
+    public final String lastName, firstName, middleName;
+    public final String salutation;
+    public final String suffix;
+    public final boolean isFemale;
+    public final boolean isEmployed;
+    public final boolean isHomeOwner;
 
-    private final Address address;
+    public final Address address;
+
+    public final List<Car> cars;
 
     /**
      * All fields constructor (REQUIRED).
@@ -153,7 +177,8 @@ class Person {
         final boolean isFemale,
         final boolean isEmployed,
         final boolean isHomeOwner,
-        final Address address) {
+        final Address address,
+        final List<Car> cars) {
 
         this.lastName = lastName;
         this.firstName = firstName;
@@ -164,6 +189,7 @@ class Person {
         this.isEmployed = isEmployed;
         this.isHomeOwner = isHomeOwner;
         this.address = address;
+        this.cars = cars;
     }
 
     public String lastName () {
@@ -172,12 +198,14 @@ class Person {
 
     public Person withLastName (String lastName) {
         return new Person (
-            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed, isHomeOwner, address);
+            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed,
+            isHomeOwner, address, cars);
     }
 
     public Person lastName (String lastName) {
         return new Person (
-            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed, isHomeOwner, address);
+            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed,
+            isHomeOwner, address, cars);
     }
 
     public String firstName () {
@@ -186,12 +214,14 @@ class Person {
 
     public Person withFirstName (String firstName) {
         return new Person (
-            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed, isHomeOwner, address);
+            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed,
+            isHomeOwner, address, cars);
     }
 
     public Person firstName (String firstName) {
         return new Person (
-            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed, isHomeOwner, address);
+            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed,
+            isHomeOwner, address, cars);
     }
 
     public String middleName () {
@@ -200,12 +230,14 @@ class Person {
 
     public Person withMiddleName (String middleName) {
         return new Person (
-            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed, isHomeOwner, address);
+            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed,
+            isHomeOwner, address, cars);
     }
 
     public Person middleName (String middleName) {
         return new Person (
-            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed, isHomeOwner, address);
+            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed,
+            isHomeOwner, address, cars);
     }
 
     public String salutation () {
@@ -214,12 +246,14 @@ class Person {
 
     public Person withSalutation (String salutation) {
         return new Person (
-            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed, isHomeOwner, address);
+            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed,
+            isHomeOwner, address, cars);
     }
 
     public Person salutation (String salutation) {
         return new Person (
-            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed, isHomeOwner, address);
+            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed,
+            isHomeOwner, address, cars);
     }
 
     public Address address () {
@@ -228,12 +262,26 @@ class Person {
 
     public Person withAddress (Address address) {
         return new Person (
-            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed, isHomeOwner, address);
+            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed,
+            isHomeOwner, address, cars);
     }
 
     public Person address (Address address) {
         return new Person (
-            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed, isHomeOwner, address);
+            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed,
+            isHomeOwner, address, cars);
+    }
+
+    public Person cars (List<Car> cars) {
+        return new Person (
+            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed,
+            isHomeOwner, address, cars);
+    }
+
+    public Person cars (Car... cars) {
+        return new Person (
+            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed,
+            isHomeOwner, address, asList (cars));
     }
 
     @Override public boolean equals (Object o) {
@@ -252,12 +300,14 @@ class Person {
             equal (this.isFemale, that.isFemale) &&
             equal (this.isEmployed, that.isEmployed) &&
             equal (this.isHomeOwner, that.isHomeOwner) &&
-            equal (this.address, that.address);
+            equal (this.address, that.address) &&
+            equal (this.cars, that.cars);
     }
 
     @Override public int hashCode () {
         return hash (
-            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed, isHomeOwner, address);
+            lastName, firstName, middleName, salutation, suffix, isFemale, isEmployed,
+            isHomeOwner, address, cars);
     }
 
     @Override public String toString () {
@@ -271,7 +321,8 @@ class Person {
             print ("isFemale", isFemale) +
             print ("isEmployed", isEmployed) +
             print ("isHomeOwner", isHomeOwner) +
-            print ("address", address);
+            print ("address", address) +
+            print ("cars", cars);
     }
 }
 
@@ -283,31 +334,54 @@ public class InmutableExample {
         out.println (person ()
             .withFirstName ("Juanjo")
             .withAddress (address ()
-                .withStreetAddress ("C/Dolores Folgueras")
-                .withCity ("Madrid")
+                    .withStreetAddress ("C/Dolores Folgueras")
+                    .withCity ("Madrid")
             )
         );
         out.println (person (p -> {
             p = p.withFirstName ("Juanjo");
 
             return p.withAddress (address ()
-                .withStreetAddress ("C/Dolores Folgueras")
-                .withCity ("Madrid")
+                    .withStreetAddress ("C/Dolores Folgueras")
+                    .withCity ("Madrid")
             );
         }));
         out.println (person ()
             .firstName ("Juanjo")
             .address (address ()
-                .streetAddress ("C/Dolores Folgueras")
-                .city ("Madrid")
+                    .streetAddress ("C/Dolores Folgueras")
+                    .city ("Madrid")
             )
         );
         out.println (person ().
             firstName ("Juanjo").
             address (address ().
-                streetAddress ("C/Dolores Folgueras").
-                city ("Madrid")
+                    streetAddress ("C/Dolores Folgueras").
+                    city ("Madrid")
             )
         );
+
+        out.println (person ().
+                firstName ("Juanjo").
+                address (address ().
+                        streetAddress ("C/Dolores Folgueras").
+                        city ("Madrid")
+                ).
+                cars (
+                    car ("Seat", "Ibiza"),
+                    car ("Porsche", "911")
+                )
+        );
+
+        Person p = person ().
+            firstName ("Juanjo").
+            address (address ().
+                streetAddress ("C/Dolores Folgueras").
+                city ("Madrid").
+                state ("Madrid")
+            );
+
+        out.println (p.withFirstName (p.firstName () + " Jr."));
+        out.println (p.firstName (p.firstName + " Jr."));
     }
 }
